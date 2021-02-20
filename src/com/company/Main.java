f9c8c93051e9bbef68c9da2dd744f3f6dc02578d
Java02 <Java02@it-redmonkey.com>
2021-02-20 13:53:55 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int [] array = {10, 20, 30, 40, 50};
+	// 20 30 40 50 10
+        for (int i = 0; i < array.length; i++) {
+        }
+        array[i]
+    }
+}
